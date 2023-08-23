b1c041a9e7a93d25b6e5e6ec87b9920651dba934
gimmingyu <gimmingyu0201@gmail.com>
2023-08-24 02:32:38 +0900

    feat: add session.go

--- internal/private/session.go
+++ internal/private/session.go
@@ -1,1 +1,14 @@
 package private
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func Session(accessKey, secretKey string) (*session.Session, error) {
+	return session.NewSession(&aws.Config{Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+		AccessKeyID:     accessKey,
+		SecretAccessKey: secretKey,
+	})})
+}
